bf84dedf109a234c17682ff2121041bf7286d2af
polonusg <polonusg@hnicorp.com>
2020-03-11 08:18:33 -0500

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,121 @@
+
+const mvc = window.mvc || {
+  run: () => {},
+  loop: () => {}
+};
+
+const math = window.math
+
+const start = (
+  canvas,
+  xInput,
+  xMinInput,
+  xMaxInput,
+  yMinInput,
+  yMaxInput,
+) => {
+  const {
+    offsetWidth: pw,
+    offsetHeight: ph
+  } = document.querySelector('body')
+
+  // setting up the canvas
+  const ctx = canvas.getContext("2d");
+  ctx.canvas.height = ph;
+  ctx.canvas.width = pw;
+
+  // use the framework to setup the state pipeline
+  const triggers = mvc.run(
+    state,
+    actions,
+    render(ctx, pw, ph)
+  )
+
+  // get the function that will change a key of state
+  const stateChanger = changer(triggers.changeState)
+
+  // setup the elements to be looped over
+  const elements = {
+    x: xInput,
+    f: fInput,
+    xMin: xMinInput,
+    xMax: xMaxInput,
+    yMin: yMinInput,
+    yMax: yMaxInput,
+  }
+
+  // assign the event listeners
+  Object.entries(elements).map(
+    ([ key, element ]) =>
+      addEventListener(element, 'change', stateChanger(key))
+  )
+}
+
+const addEventListener = (element, type, func) => {
+  element.addEventListener(type, func)
+  return () => element.removeEventListener('type', func)
+}
+
+const changer = changeState => key => ({ target: { value } }) => changeState(key, value)
+
+const state = {
+  f: 'x',
+  xMIn: -10,
+  xMax: 10,
+  yMin: -10,
+  yMax: 10
+}
+
+const actions = {
+  changeState: (key, data) => s => ({
+    [key]: data,
+    ...s
+  }),
+}
+
+const pointToCanvas = (pw, ph) => (x, y, xMin, xMax, yMin, yMax) => [ (x - xMin) / (xMax - xMin) * pw, (y - yMin) / (yMax - yMin) * ph ]
+
+const render = (ctx, pw, ph) => ({ f, xMin, xMax, yMin, yMax }, a) => {
+  // *** clear the canvas
+  ctx.clearRect(0, 0, pw, ph)
+
+  // *** draw the axes
+  ctx.lineWidth = 2;
+  ctx.beginPath()
+  ctx.moveTo(pw / 2, 0)
+  ctx.lineTo(pw / 2, ph)
+  ctx.moveTo(0, ph / 2)
+  ctx.lineTo(pw, ph / 2)
+  ctx.stroke()
+
+  // *** TODO: draw the labels
+
+
+  // *** draw the points
+  const numberOfPoints = 1000;
+  const plotPoint = pointToCanvas(pw, ph)
+  const evalF = x => math.evaluate(f, { x })
+  const delta = (xMax - xMin) / numberOfPoints
+  Array(numberOfPoints).fill().map((_, i) => {
+    const x = xMin + i * delta
+    const y = evalF(x)
+    const [cx, cy] = plotPoint(x, y, xMin, xMax, yMin, yMax)
+    ctx.fillRect(cx - 2, cy - 2, 4, 4)
+  })
+}
+
+
+/**
+ * RUN THE THING
+ */
+((canvas, fInput, xInput)=>{
+  start(canvas, fInput, xInput)
+})(
+  document.getElementById('canvas'),
+  document.getElementById('f-input'),
+  document.getElementById('x-input'),
+  document.getElementById('x-min-input'),
+  document.getElementById('x-max-input'),
+  document.getElementById('y-min-input'),
+  document.getElementById('y-max-input'),
+)
---
+++ src/mvc.js
@@ -0,0 +1,89 @@
+/** three pieces
+- state
+  - one of these
+- state change triggers
+  - many of these
+- actions
+  - functions with passed in state that change the data and return state again
+  - purely functional system of functions
+- render
+  - one function that renders the whole thing out based on one set of state
+
+state -> model
+view -> render
+actions -> control
+
+render could be on a loop or called after state updates
+
+*/
+
+
+/**
+ * State closure and state mutation function
+ * @param {any} state Initial State
+ * @param {object: { [key]: ([data]) => (state) => newState }} actions Object holding functions that return functions that mutate state based on new data and current state
+ * @param {function} render Function that takes state and does something based on state. Will be called after every state mutation
+ * Returns
+ *  object holding state mutation functions
+*/
+function run(state, actions, render) {
+  let s = state
+  const a = Object.entries(actions).reduce((ac, [name, action]) => ({
+    ...ac,
+    [name]: (...d) => {
+      s = action(...d)(s)
+      render(s, a)
+    }
+  }), {})
+  return a
+}
+
+/**
+ * State closure with mutations on a loop
+ * @param {any} state Initial State
+ * @param {object: { [key]: ([data]) => (state) => newState }} actions Object holding functions that return functions that mutate state based on new data and current state
+ * @param {function} render Function that takes state and does something based on state. Will be called after every {time} milliseconds
+ * @param {number} time How many milliseconds between calls
+ * @param {string} every Key of action function that will be called before every render
+ *
+ * Returns
+ * [
+ *  object holding state mutation functions,
+ *  start function,
+ *  stop function
+ * ]
+ */
+function loop(state, actions, render, time, every) {
+  let s = state
+  const a = Object.entries(actions).reduce((ac, [name, action]) => ({
+    ...ac,
+    [name]: (...d) => {
+      s = action(...d)(s)
+    }
+  }), {})
+
+  every = (every && a[every]) || (() => 0)
+  let fun
+  const repeat = () => {
+    every(s)
+    render(s)
+    setTimeout(fun, time)
+  }
+
+  return [
+    a,
+    () => {
+      fun = repeat
+      setTimeout(fun, time)
+    },
+    () => {
+      fun = () => {}
+    }
+  ]
+}
+
+// export the good stuff
+window.mvc = window.mvc || {
+  run,
+  loop
+}
